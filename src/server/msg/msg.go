eda7f2a3194405a97c0174c1165dcf5f645ce143
potwasclosed <1683721999@qq.com>
2022-01-08 23:23:24 +0800

    Update msg.go

--- src/server/msg/msg.go
+++ src/server/msg/msg.go
@@ -19,3 +19,32 @@ func init() {
 //}
 
 //chanrpc的一个容器可以放到一个单独的结构中维护,map[id/chanName]chanRpc而不是直接引用那个模块的变量这样不容易循环依赖.
+
+/*
+这个还可以再封装下 api更好用.
+func handleRpcMsg(f func(interface{}), s *chanrpc.Server) func(interface{}) {
+	return func(_ret interface{}) {
+		type AsyncRet struct {
+			cb  interface{}
+			ret interface{}
+		}
+		asyncRet := &AsyncRet{
+			cb:  f,
+			ret: _ret,
+		}
+		s.Go(asyncRet)
+		//A协程call B进程里返回结果,回调函数人在A协程里执行, 把回调和返回值 重新打包投递到chanrpc里执行
+		//这个方法适合多  协程跨进程调用别的多协程.
+	}
+}
+
+example := func() {
+	hallses := service.GetRemoteService("hallBackend#1@zone1")
+	req := &ssmsg.ServerCallReq{}
+	_cb := func(ret interface{}) {
+		//回调需要做的事情
+	}
+	rpc.Call(hallses, req, time.Second*30, handleRpcMsg(_cb, MS[0].ChanRPCServer))
+}
+	_ = example
+*/
